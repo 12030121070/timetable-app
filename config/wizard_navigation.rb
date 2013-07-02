26fdf3418681cdc8eb0533f9b6d3671b55aa18b5
tab <tukhfatullin@gmail.com>
2013-07-02 16:18:58 +0700

    Fix navigation

--- config/wizard_navigation.rb
+++ config/wizard_navigation.rb
@@ -1,10 +1,14 @@
 SimpleNavigation::Configuration.run do |navigation|
   navigation.items do |primary|
     primary.item :workplace, 'Workplace', workplace_root_path
-    primary.item :organizations, 'Create organizations', new_workplace_organization_path
-    primary.item :classrooms, 'Create classrooms', '#'
-    primary.item :lecturer, 'Create lecturer', '#'
-    primary.item :membership, 'Create membership', '#'
-    primary.item :timetables, 'Create timetables', '#'
+    if current_user.organizations.empty?
+      primary.item :organizations, 'Create organizations', new_workplace_organization_path
+    end
+    primary.item :organization, @organization.title, workplace_organization_path(@organization) do |org|
+      org.item :classrooms, 'Create classrooms', new_workplace_organization_building_path(@organization)
+      org.item :lecturer, 'Create lecturer', new_workplace_organization_lecturer_path(@organization)
+      org.item :membership, 'Create membership', workplace_organization_memberships_path(@organization)
+      org.item :timetables, 'Create timetables', '#'
+    end if @organization
   end
 end
