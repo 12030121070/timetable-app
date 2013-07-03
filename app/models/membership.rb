cb187090354c5bf1ee919fbe4db4a5923d91f6b6
Evgeniy Serykh <e.v.serykh@gmail.com>
2013-07-03 09:05:03 +0700

    Non active memberships

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,13 +1,17 @@
 class Membership < ActiveRecord::Base
   extend Enumerize
 
-  attr_accessible :role, :user_id
+  attr_accessible :role, :user_id, :email
 
   belongs_to :user
   belongs_to :organization
 
   validates_presence_of :role
-  validates_uniqueness_of :role, :scope => [:user_id, :organization_id]
+  #validates_uniqueness_of :role, :scope => [:user_id, :email, :organization_id]
 
   enumerize :role, :in => [:admin, :member]
+
+  def activated?
+    user_id?
+  end
 end
