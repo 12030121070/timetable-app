05b339e296a92bc3b05b6c9719025796278b61f2
tab <tukhfatullin@gmail.com>
2013-07-05 11:38:28 +0700

    Add hm to lesson

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -4,4 +4,13 @@ class Lesson < ActiveRecord::Base
   belongs_to :day
   belongs_to :discipline
   belongs_to :lesson_time
+
+  has_many :classroom_lessons, :dependent => :destroy
+  has_many :classrooms, :through => :classroom_lessons
+
+  has_many :group_lessons, :dependent => :destroy
+  has_many :groups, :through => :group_lessons
+
+  has_many :lecturer_lessons, :dependent => :destroy
+  has_many :lecturers, :through => :lecturer_lessons
 end
