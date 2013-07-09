8317625f656e9d926aaf620396d3b97cf49ababf
Evgeniy Serykh <e.v.serykh@gmail.com>
2013-07-09 10:10:52 +0700

    Копирование урока :dizzy:

---
+++ app/controllers/workplace/lesson_copies_controller.rb
@@ -0,0 +1,28 @@
+class Workplace::LessonCopiesController < Workplace::WorkplaceController
+  inherit_resources
+
+  actions :new, :create
+
+  belongs_to :organization, :timetable, :week, :day, :lesson
+
+  defaults :resource_class => false
+
+  def create
+    create! do
+      params[:cells].each do |cell|
+        day, lesson_time = Day.find(cell.split('_').first), LessonTime.find(cell.split('_').last)
+        @lesson.copy_to(day, lesson_time)
+      end
+
+      redirect_to [:workplace, @organization, @timetable, @week] and return
+    end
+  end
+
+  protected
+
+  def build_resource
+  end
+
+  def create_resource(object)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,11 @@ TimetableApp::Application.routes.draw do
   namespace :workplace do
     resources :organizations, :except => [:index] do
       resources :disciplines, :except => :show
+
       resources :lecturers, :except => [:show, :index] do
         post :import, :on => :collection
       end
+
       resources :memberships, :except => [:show, :index]
 
       resources :buildings, except: [:show, :index] do
@@ -15,17 +17,20 @@ TimetableApp::Application.routes.draw do
       end
 
       resources :timetables do
-        put 'to_published'  => 'timetables#to_published', :on => :member
-        put 'to_draft'  => 'timetables#to_draft', :on => :member
+        put 'to_draft'     => 'timetables#to_draft',     :on => :member
+        put 'to_published' => 'timetables#to_published', :on => :member
+
         resources :groups, except: [:index, :show]
         resources :lesson_times
+
         resources :weeks, except: [:index, :new, :edit] do
-          get 'copy/new', :to => 'copy_week#new', :as => :new_copy
-          post 'copy', :to => 'copy_week#create', :as => :copy
+          get 'copy/new', :to => 'copy_week#new',    :as => :new_copy
+          post 'copy',    :to => 'copy_week#create', :as => :copy
+
           resources :days, only: [] do
             resources :lessons, except: [:index, :show] do
-              get 'to_copy', :on => :member
-              put 'copy', :on => :member
+              get  'copies/new' => 'lesson_copies#new',    :as => :new_copy
+              post 'copies'     => 'lesson_copies#create', :as => :copies
             end
           end
         end
