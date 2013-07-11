43f243462d3d7c521ad656678c56d367516fe905
Evgeniy Serykh <e.v.serykh@gmail.com>
2013-07-11 16:50:55 +0700

    Печать расписания (начальный вариант)

--- app/models/lecturer.rb
+++ app/models/lecturer.rb
@@ -13,5 +13,9 @@ class Lecturer < ActiveRecord::Base
   def full_name
     "#{surname} #{name} #{patronymic}"
   end
-  alias_method :to_s, :full_name
+
+  def short_name
+    "#{surname.mb_chars.capitalize} #{name.first.mb_chars.capitalize}. #{patronymic.first.mb_chars.capitalize}."
+  end
+  alias_method :to_s, :short_name
 end
