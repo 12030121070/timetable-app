abdcbc4d39a7ffe502e94543378f6d9bdb0fb3f8
Manin Iliy <bkmzvf@gmail.com>
2013-08-22 15:13:16 +0700

    added title for menu item

--- config/navigation.rb
+++ config/navigation.rb
@@ -3,14 +3,11 @@
 SimpleNavigation::Configuration.run do |navigation|
   navigation.items do |side_nav|
     if @organization.try(:persisted?)
-      side_nav.item :timetables,  '<span></span><b>Расписания</b>', workplace_timetables_path,    :highlights_on => -> { ['timetables', 'weeks'].include?(controller_name) }
-      side_nav.item :members,     '<span></span><b>Менеджеры</b>',  workplace_memberships_path,   :highlights_on => -> { controller_name == 'memberships' }
-      side_nav.item :lecturers,   '<span></span><b>Лекторы</b>',    workplace_lecturers_path,     :highlights_on => -> { controller_name == 'lecturers' }
-      side_nav.item :buildings,   '<span></span><b>Корпуса и аудитории</b>',    workplace_buildings_path,     :highlights_on => -> { controller_name == 'buildings' }
-      side_nav.item :disciplines, '<span></span><b>Дисциплины</b>', workplace_disciplines_path,   :highlights_on => -> { controller_name == 'disciplines' }
-      side_nav.item :separator,   '', "#"
-      side_nav.item :lecturers_loading, '<span></span><b>Загрузка лекторов</b>', '#'
-      side_nav.item :buildings_loading, '<span></span><b>Загрузка аудиторий</b>', '#'
+      side_nav.item :timetables,  '<span title="Расписание"></span><b>Расписания</b>', workplace_timetables_path,    :highlights_on => -> { ['timetables', 'weeks'].include?(controller_name) }
+      side_nav.item :members,     '<span title="Менеджеры"></span><b>Менеджеры</b>',  workplace_memberships_path,   :highlights_on => -> { controller_name == 'memberships' }
+      side_nav.item :lecturers,   '<span title="Лекторы"></span><b>Лекторы</b>',    workplace_lecturers_path,     :highlights_on => -> { controller_name == 'lecturers' }
+      side_nav.item :buildings,   '<span title="Корпуса т аудитории"></span><b>Корпуса и аудитории</b>',    workplace_buildings_path,     :highlights_on => -> { controller_name == 'buildings' }
+      side_nav.item :disciplines, '<span title="Дисциплины"></span><b>Дисциплины</b>', workplace_disciplines_path,   :highlights_on => -> { controller_name == 'disciplines' }
     end
   end
 end
