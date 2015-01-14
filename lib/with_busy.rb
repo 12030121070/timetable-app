9e8a48eb321637a76c66e29a7f81ab2a8532163d
Evgeniy Serykh <e.v.serykh@gmail.com>
2015-01-14 11:32:56 +0600

    #10 - Уроки по подгруппам

--- lib/with_busy.rb
+++ lib/with_busy.rb
@@ -6,20 +6,56 @@ module WithBusy
       method, message = options[:method], options[:message]
 
       define_method "#{method}_with_busy" do |day, lesson_time, lesson|
-        send(method).tap { |s| s << " (#{message})" if busy_at?(day, lesson_time, lesson) }
+        if self.is_a?(Group)
+          if busy_at?(day, lesson_time, lesson)
+            if has_lessons_with_subgroup_at?(day, lesson_time)
+              "#{send(method)} (уже есть занятие в подгруппе #{lessons_with_subgroup_at(day, lesson_time).first.subgroup_text})"
+            else
+              "#{send(method)} (#{message})"
+            end
+          else
+            send(method)
+          end
+        else
+          send(method).tap { |s| s << " (#{message})" if busy_at?(day, lesson_time, lesson) }
+        end
       end
 
       define_method "autocomplete_#{method}_with_busy" do |day, lesson_time, lesson|
-        send(method).tap { |s| s << " (#{message})" if has_lessons_at?(day, lesson_time) }
+        if self.is_a?(Group)
+          if has_lessons_at?(day, lesson_time)
+            if has_lessons_with_subgroup_at?(day, lesson_time)
+              "#{send(method)} (уже есть занятие в подгруппе #{lessons_with_subgroup_at(day, lesson_time).first.subgroup_text})"
+            else
+              "#{send(method)} (#{message})"
+            end
+          else
+            send(method)
+          end
+        else
+          send(method).tap { |s| s << " (#{message})" if has_lessons_at?(day, lesson_time) }
+        end
       end
     end
   end
 
   private
 
-  def has_lessons_at?(day, lesson_time)
+  def lessons_at(day, lesson_time)
     lessons.joins(:day).where('days.date = ?', day.date)
-      .joins(:lesson_time).where('lesson_times.day = ? AND lesson_times.number = ?', day.cwday, lesson_time.number).any?
+      .joins(:lesson_time).where('lesson_times.day = ? AND lesson_times.number = ?', day.cwday, lesson_time.number)
+  end
+
+  def lessons_with_subgroup_at(day, lesson_time)
+    lessons_at(day, lesson_time).where('lessons.subgroup != ?', :whole)
+  end
+
+  def has_lessons_with_subgroup_at?(day, lesson_time)
+    lessons_with_subgroup_at(day, lesson_time).any?
+  end
+
+  def has_lessons_at?(day, lesson_time)
+    lessons_at(day, lesson_time).any?
   end
 
   def busy_at?(day, lesson_time, lesson)
