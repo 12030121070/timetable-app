b9f8c167b37c718637227cff2907d9fd4705e331
Manin Iliy <bkmzvf@gmail.com>
2015-01-19 11:13:28 +0600

    lecturer statistic

--- lib/lecturer_statistic.rb
+++ lib/lecturer_statistic.rb
@@ -1,5 +1,4 @@
 class LecturerStatistic
-  include LessonsFor
   attr_accessor :data, :lecturer, :timetable_ids
 
   def initialize(lecturer, timetable_ids)
@@ -10,33 +9,35 @@ class LecturerStatistic
     self
   end
 
-  def timetables
+  def lessons
     if timetable_ids
-      @timetables ||= lecturer.timetables.where(:id => timetable_ids.to_a.compact.map(&:to_i))
+      @lessons ||= lecturer.lessons.joins(:timetable).where(:timetables => { :id => timetable_ids })
     else
-      @timetables ||= lecturer.timetables
+      @lessons ||= lecturer.lessons.joins(:timetable)
     end
   end
 
   def weeks
-    @weeks ||= timetables.flat_map(&:weeks).map(&:number).uniq
+    @weeks ||= lessons.flat_map(&:week).map(&:number).uniq
+  end
+
+  def groups
+    @groups ||= lessons.flat_map(&:groups).uniq
   end
 
   private
 
   def fill_data
-    timetables.flat_map(&:groups).sort_by(&:title).each do |group|
-      data[group] = {}
-      group.disciplines.each do |discipline|
-        data[group][discipline.title] = {}
-        lessons_for(group, discipline).map(&:kind).each do |k|
-          data[group][discipline.title][kinds(k)] = {}
-          weeks.each do |w|
-            data[group][discipline.title][kinds(k)][w] = lessons_for(group, discipline, week: group.timetable.weeks.find_by_number(w), kind: k).count
-          end
-        end
+    lessons.each do |lesson|
+      lesson.groups.each do |group|
+        data[group.title] ||= {}
+        data[group.title][lesson.discipline.title] ||= {}
+        data[group.title][lesson.discipline.title][kinds(lesson.kind)] ||= {}
+        data[group.title][lesson.discipline.title][kinds(lesson.kind)][lesson.week.number] ||= 0
+        data[group.title][lesson.discipline.title][kinds(lesson.kind)][lesson.week.number] += 1
       end
     end
+    @data = data.sort.to_h
   end
 
   def kinds(kind)
