5d9ec3dbb396f0650bdf910316a439b69ab1766a
tab <tukhfatullin@gmail.com>
2015-01-26 10:45:59 +0600

    RF docs navigation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+  def docs_pages
+    {
+      'about' => 'Что такое FlipTable?',
+      'getting-started' => 'Начало работы с FlipTable',
+      'organization-settings' => 'Настройки учреждения',
+      'lecturers' => 'Справочник преподавателей',
+      'buildings' => 'Справочник аудиторного фонда',
+      'disciplines' => 'Справочник дисциплин',
+      'timetable' => 'Составление расписания',
+      'copy-move-lesson' => 'Копирование и перенос урока',
+      'copy-weeks' => 'Копирование недель',
+      'timetable-publication' => 'Публикация расписания',
+      'timetable-statistics' => 'Статистика нагрузки по расписанию',
+      'managers' => 'Многопользовательский режим работы',
+      'timetable-in-public' => 'Публичный вид расписания',
+      'subscription' => 'Подписки',
+      'support' => 'Поддержка пользователей и гарантии',
+      'contacts' => 'Контакты'
+    }
+  end
 end
